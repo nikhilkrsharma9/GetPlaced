b44a80921010da2f0cfc4aaf6c354d54b809624f
Your Name <your.email@example.com>
2025-06-07 00:19:05 +0530

    added admin verification and college rating in models

--- job_portal/portal/models.py
+++ job_portal/portal/models.py
@@ -6,12 +6,14 @@ from django.db import models
 class college (models.Model):
     college_name = models.CharField(max_length = 100, default = 'N/A')
     college_location = models.CharField(max_length = 100, default = 'N/A')
-    college_affiliation = models.CharField(max_length = 100, default = 'N/A')
+    college_university = models.CharField(max_length = 100, default = 'N/A')
     college_logo = models.ImageField(upload_to='college_logos/', default='college_logos/default.png')
     college_website = models.URLField(max_length=200, default='https://www.example.com')
     college_mobile_no = models.CharField(max_length=15, default='N/A')
     college_email = models.EmailField(max_length=100, default='N/A')
     college_description = models.TextField(default='N/A')
+    admin_verified = models.BooleanField(default=False)
+    college_rating = models.PositiveSmallIntegerField(default=0, help_text="Rating (1-5 stars)")
     
     def __str__(self):
         return self.college_name
