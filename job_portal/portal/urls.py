6696a6cedd766de2a1ead6104ed99827450291c4
Your Name <your.email@example.com>
2025-06-06 21:15:42 +0530

    added some comments

--- job_portal/portal/urls.py
+++ job_portal/portal/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('search/', views.job_search, name='job_search'),
+    path('', views.index), # index or home page
+    path('search/', views.job_search, name='job_search'),  # the seach box in index.html or home page
 ]
--- job_portal/portal/views.py
+++ job_portal/portal/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-# Create your views here.
+# it is the index or home page
 def index (request):
     return render(request, 'index.html')
 
+#it is the search box in index.html or home page
 def job_search(request):
-    # Implement your search logic here
     return render(request, 'search_results.html')
