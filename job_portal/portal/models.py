7810bc034c52d786fac0a8928f04fa933f2c2bef
Your Name <your.email@example.com>
2025-06-06 21:35:49 +0530

    made models for college details

--- job_portal/portal/models.py
+++ job_portal/portal/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+# models for college entity
+class college (models.Model):
+    college_name = models.CharField(max_length = 100, default = 'N/A')
+    college_location = models.CharField(max_length = 100, default = 'N/A')
+    college_affiliation = models.CharField(max_length = 100, default = 'N/A')
+    college_logo = models.ImageField(upload_to='college_logos/', default='college_logos/default.png')
+    college_website = models.URLField(max_length=200, default='https://www.example.com')
+    college_mobile_no = models.CharField(max_length=15, default='N/A')
+    college_email = models.EmailField(max_length=100, default='N/A')
+    college_description = models.TextField(default='N/A')
+    class Meta:
+        db_table = "college"
+        
