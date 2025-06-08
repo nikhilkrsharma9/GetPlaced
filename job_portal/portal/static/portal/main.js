889e6e845e01df29c901e478e49524056ae086af
Your Name <your.email@example.com>
2025-06-08 21:26:05 +0530

    improved index page css

--- job_portal/portal/static/portal/main.js
+++
@@ -1,9 +0,0 @@
-// Dropdown for Login and Register
-document.querySelectorAll('nav > div').forEach(function(drop){
-    drop.addEventListener('mouseenter', function(){
-        this.querySelector('div').style.display = 'block';
-    });
-    drop.addEventListener('mouseleave', function(){
-        this.querySelector('div').style.display = 'none';
-    });
-});
